2afc6db9397b1e468b66ba8d523b9d3c2b84816b
p4tr1ck <patricknicholas@foxmail.com>
2021-12-09 18:48:46 +0800

    add helper for add-index test, fix my_exec

--- quantify/stability/ycsb/add-index.bash
+++ quantify/stability/ycsb/add-index.bash
@@ -1,4 +1,5 @@
 set -euo pipefail
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../../helper/helper.bash"
 
 session="${1}"
 env=`cat "${session}/env"`
@@ -7,7 +8,7 @@ host=`must_env_val "${env}" 'mysql.host'`
 port=`must_env_val "${env}" 'mysql.port'`
 user=`must_env_val "${env}" 'mysql.user'`
 
-function my_exe()
+function my_exec()
 {
 	local query="${1}"
 	mysql -h "${host}" -P "${port}" -u "${user}" --database=test -e "${query}"
